e02de869ebad9be3ea5cf1e607915a98f4125089
Renan Verissimo de V. Souza <souza.sup.erp@alterdata.matriz>
2016-12-16 12:29:45 -0200

    Code improve and minor bugs fixed.
    - Proxy ajusted;
    
    - Better use of Promisses and a chain between then.

--- TReturnerConsum/TReturnerConsum.js
+++ TReturnerConsum/TReturnerConsum.js
@@ -1,55 +1,48 @@
 'use strict';
 
 angular.module('ControleConsulta')
-    .factory('localizaId', function($http, apiConfig) {
-        var localizaId = function(id){
-            this.id = id;
-            this.data = null;
-            this.exception = null;
-            this._consultar();
-        };
+    .factory('localizaId', function($http, $q, apiConfig) {
+        var localizaId = function(){}
 
-        localizaId.prototype._consultar = function () {
+        localizaId.prototype._consultar = function(id){
 
-             $http({
+             return $http.get(apiConfig.apiBase + 'trequest/' + id)
+            .then((response) =>{
 
-                method: 'GET',
-                url: apiConfig.apiBase + '/trequest/' + this.id
+              return response.data;   
+            }).catch((err) =>{
 
-            }).then((response) =>{
-                
-                this.data = response.data;
-                
-            }).catch((error) =>{
-
-                this.exception = error.status;
+                return $q.reject(err.status);
             })
         }
 
-        return localizaId;
+        return new localizaId();
     })
 
     .controller('TReturnerConsum', function($scope, localizaId) {
+
         var recebeTs = {}
 
         $scope.consultar = () => {
-            var lid = new localizaId($scope.id);
-             
-            if(lid.exception != null){
 
-                if(lid.exception === -1){
+            var returnPromisse = localizaId._consultar($scope.id)
+            .then((res)=>{
+                
+                return $scope.recebeTs = res;
+            })
+            .catch((err)=>{
+
+                if(err === -1){
                     return alert("Falha ao conectar com o EndPoint!");
                 }
-
-                return alert("Erro 404 -  Id não Encontrado!");
-            }
-
-            $scope.recebeTs = lid.data;
-        }
+            
+            return alert("Erro 404 -  Id não Encontrado!");
+        })        
+    }
 
         $scope.limpar = () =>{
 
-            $scope.id = null;
-            $scope.recebeTs = null;
+            $scope.id = "";
+            $scope.recebeTs = "";
         }
     })
